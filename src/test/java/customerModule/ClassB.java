d08ea6d7a6a9c30b42d1be8631581b13a57909fb
akashdeb <62165769+akashdeb@users.noreply.github.com>
2023-07-18 15:51:34 +0530

    Update ClassB.java

--- src/test/java/customerModule/ClassB.java
+++ src/test/java/customerModule/ClassB.java
@@ -5,7 +5,7 @@ public class ClassB {
 	public static void main(String[] args) {
 		System.out.println("Demo Print");
 
-		System.out.println("Test Print");
+		System.out.println("Real Print");
 	}
 
 }
